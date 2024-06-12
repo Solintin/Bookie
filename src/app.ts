7f16b8c05da81634e4b5958fb721f1d1d7e0258c
DevSaalih <soliualley@gmail.com>
2024-06-12 18:51:45 +0100

    Update Error Response message

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.get("/hello", (req, res) => {
 app.use("*", (_, res: Response) => {
   return res.status(StatusCodes.NOT_FOUND).json({
     success: false,
-    message: "Route not found",
+    message: "Route not found, get Out",
   });
 });
 app.use((err, req, res, next) => {
