0b6ccce46d6300a95fdba6bceb8b4b2999b85c1f
DevSaalih <soliualley@gmail.com>
2024-06-17 01:30:42 +0100

    Update Data-source by add cond statemrnt check env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -12,11 +12,11 @@ console.log(isLocal);
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USERNAME || "user",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "bookie",
+  host: isLocal ? process.env.DB_HOST : "127.0.0.1",
+  port: isLocal ? Number(process.env.DB_PORT) : 3306,
+  username: isLocal ? process.env.DB_USERNAME : "user",
+  password: isLocal ? process.env.DB_PASSWORD : "root",
+  database: isLocal ? process.env.DB_DATABASE : "bookie",
   logging: true,
   synchronize: false,
   ssl: {
